3e673e823bdc46e096b095b68bdedad9b1be73dc
Per Stark <perstark.se@gmail.com>
2024-10-15 13:29:57 +0200

    refactoring to surrealdb

--- src/routes/ingress.rs
+++ src/routes/ingress.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
 use tracing::{error, info};
-use crate::{models::ingress_content::{create_ingress_objects, IngressInput}, rabbitmq::publisher::RabbitMQProducer, redis::client::RedisClient};
+use crate::{models::{file_info::FileError, ingress_content::{create_ingress_objects, IngressInput}}, rabbitmq::publisher::RabbitMQProducer, redis::client::RedisClient, surrealdb::SurrealDbClient};
 
 pub async fn ingress_handler(
     Extension(producer): Extension<Arc<RabbitMQProducer>>,
@@ -9,9 +9,10 @@ pub async fn ingress_handler(
 ) -> impl IntoResponse {
     info!("Received input: {:?}", input);
 
+    let db_client = SurrealDbClient::new().await.map_err(|e| FileError::PersistError(e.to_string())).unwrap();
     let redis_client = RedisClient::new("redis://127.0.0.1/");
 
-    match create_ingress_objects(input, &redis_client).await {
+    match create_ingress_objects(input, &db_client).await {
         Ok(objects) => {
             for object in objects {
                 match producer.publish(&object).await {
