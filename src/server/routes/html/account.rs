bdf7a12e9c3070a10c6a988da43c11b2441e028e
Per Stark <perstark.se@gmail.com>
2024-12-28 01:00:16 +0100

    feat: early redirects

--- src/server/routes/html/account.rs
+++ src/server/routes/html/account.rs
@@ -1,18 +1,18 @@
 use axum::{
     extract::State,
-    http::{Response, StatusCode},
+    http::{Response, StatusCode, Uri},
     response::{Html, IntoResponse, Redirect},
 };
+use axum_htmx::HxRedirect;
 use axum_session_auth::AuthSession;
 use axum_session_surreal::SessionSurrealPool;
 use surrealdb::{engine::any::Any, Surreal};
-use tracing::info;
 
 use crate::{
     error::ApiError,
     page_data,
     server::{routes::html::render_template, AppState},
-    storage::types::user::User,
+    storage::{db::delete_item, types::user::User},
 };
 
 use super::render_block;
@@ -25,17 +25,15 @@ pub async fn show_account_page(
     State(state): State<AppState>,
     auth: AuthSession<User, String, SessionSurrealPool<Any>, Surreal<Any>>,
 ) -> Result<impl IntoResponse, ApiError> {
-    if !auth.is_authenticated() {
-        return Ok(Redirect::to("/").into_response());
-    }
-
-    info!("{:?}", auth.current_user);
+    // Early return if the user is not authenticated
+    let user = match auth.current_user {
+        Some(user) => user,
+        None => return Ok(Redirect::to("/").into_response()),
+    };
 
     let output = render_template(
         AccountData::template_name(),
-        AccountData {
-            user: auth.current_user.unwrap(),
-        },
+        AccountData { user },
         state.templates,
     )?;
 
@@ -47,15 +45,17 @@ pub async fn set_api_key(
     auth: AuthSession<User, String, SessionSurrealPool<Any>, Surreal<Any>>,
 ) -> Result<impl IntoResponse, ApiError> {
     // Early return if the user is not authenticated
-    let user = auth.current_user.ok_or(ApiError::AuthRequired)?;
+    let user = auth.current_user.as_ref().ok_or(ApiError::AuthRequired)?;
 
     // Generate and set the API key
     let api_key = User::set_api_key(&user.id, &state.surreal_db_client).await?;
 
+    auth.cache_clear_user(user.id.to_string());
+
     // Update the user's API key
     let updated_user = User {
         api_key: Some(api_key),
-        ..user
+        ..user.clone()
     };
 
     // Render the API key section block
@@ -67,4 +67,20 @@ pub async fn set_api_key(
     )?;
 
     Ok(output.into_response())
+}
+
+pub async fn delete_account(
+    State(state): State<AppState>,
+    auth: AuthSession<User, String, SessionSurrealPool<Any>, Surreal<Any>>,
+) -> Result<impl IntoResponse, ApiError> {
+    // Early return if the user is not authenticated
+    let user = auth.current_user.as_ref().ok_or(ApiError::AuthRequired)?;
+
+    delete_item::<User>(&state.surreal_db_client, &user.id).await?;
+
+    auth.logout_user();
+
+    auth.session.destroy();
+
+    Ok((HxRedirect::from(Uri::from_static("/")), StatusCode::OK).into_response())
 }
